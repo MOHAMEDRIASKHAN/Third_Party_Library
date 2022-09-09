92b2d84c016785d3cbf6425f933ffb8bd448009b
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-09 09:49:41 -0700

    Welcome Message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+namespace ThirdPartyLibrary
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Third Party Library");
+        }
+    }
+}
